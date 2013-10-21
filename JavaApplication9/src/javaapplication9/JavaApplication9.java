2bc0799d2bb17542a1da96e74c6d8c7b16344e44
Virus <Virus@VirusPC>
2013-10-21 23:22:25 +0200

    zadanie domowe

--- JavaApplication9/src/javaapplication9/JavaApplication9.java
+++ JavaApplication9/src/javaapplication9/JavaApplication9.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package javaapplication9;
 
@@ -12,11 +7,12 @@ package javaapplication9;
  */
 public class JavaApplication9 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-      //  nowy pliczek
+      public static void main(String[] args) {
+        CD plytka = new CD();
+        AudioCD audio = new AudioCD();
+        MovieCD movie = new MovieCD();
+        movie.setTitle("Blah blah blah");
+        movie.toString();
+             
     }
-    
 }
